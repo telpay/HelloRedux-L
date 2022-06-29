ce2f61dce398aac4b7af796ebee6f01296dd8c9a
YChi LU <inec@users.noreply.github.com>
2022-06-29 07:39:50 -0600

    Initial Commit

---
+++ HelloRedux/HelloReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloReduxApp.swift
+//  HelloRedux
+//
+//  Created by Chi on 2022-06-29.
+//
+
+import SwiftUI
+
+@main
+struct HelloReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
